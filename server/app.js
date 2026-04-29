b53c13a56b3f1693fd8f73502c11af6d5713c7ab
Musab Sayyed <way2musabsayyed@gmail.com>
2026-04-29 18:40:24 +0530

    setup server

---
+++ server/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
